02f0a76afcbc0f03c26dbe41003a411bc4bc7391
vineet-alpha <kumarvineet@iiitdmj.ac.in>
2019-10-24 11:20:45 +0530

    hi code herre

---
+++ MyNewCode/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+
+
+namespace AzureTableStorage
+{
+    class Program
+    {
+        static CloudStorageAccount storageAccount;
+        static CloudTableClient tableClient;
+        static CloudTable table;
+        static void Main(string[] args)
+        {
+            try
+            {
+                CreateAzureStorageTable();
+            }
+            catch
+            {
+
+            }
+
+        }
+
+
+        private static void CreateAzureStorageTable()
+        {
+            storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageCon"));
+
+
+        }
+
+
+
+
+    }
+}
